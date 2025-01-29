8dd1c424eea4de028d48118cc67e07f97e06e70d
Bram Basha <bashabram@leerling.sint-rembert.be>
2025-01-29 10:07:43 +0100

    planeet

--- Gewicht in space/Program.cs
+++ Gewicht in space/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Gewicht_in_space
+﻿using System.Numerics;
+
+namespace Gewicht_in_space
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double gewichtOpAarde =1586 ;
+            Console.WriteLine($"Je weegt op Mercurius {gewichtOpAarde *0.38} N.");
+            Console.WriteLine($"Je weegt op Venus {gewichtOpAarde * 0.91} N.");
+            Console.WriteLine($"Je weegt op Aarde {gewichtOpAarde * 1}N.");
+            Console.WriteLine($"Je weegt op Mars {gewichtOpAarde * 0.38} N.");
+            Console.WriteLine($"Je weegt op Jupiter {gewichtOpAarde * 2.34} N.");
+            Console.WriteLine($"Je weegt op Saturnus {gewichtOpAarde * 1.06} N.");
+            Console.WriteLine($"Je weegt op Uranus {gewichtOpAarde * 0.92} N.");
+
+
+
+
         }
     }
 }
