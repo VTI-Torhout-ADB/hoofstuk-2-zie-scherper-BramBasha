97d56a13672fd5e489a7192d06352f63e541d54c
Bram Basha <bashabram@leerling.sint-rembert.be>
2025-01-29 09:51:25 +0100

    btw

--- BTW/Program.cs
+++ BTW/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace BTW
+﻿using System.Reflection;
+
+namespace BTW
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double prijs = 20;
+            const double BTW = 21.0;
+            double totaal = prijs * BTW;
+            Console.WriteLine($"{prijs} 20 euro zonder btw.Met BTW:{totaal}  euro.)
         }
     }
 }
